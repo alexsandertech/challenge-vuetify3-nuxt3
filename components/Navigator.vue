64ee1abaff2cb133504de149b9a7b31b351f83ed
Alexsander Barbosa <alexsander.barbosa@hotmail.com>
2023-08-08 19:01:27 -0300

    feat: layout with menu

---
+++ components/Navigator.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="navigator">
+      <NavLink
+        v-for="link in navLinks"
+        :key="link.path"
+        :to="link.path"
+      >
+        {{ link.label }}
+      </NavLink>
+    </nav>
+</template>
+  
+<script setup lang="ts">
+    import NavLink from './NavLink.vue';
+    import { INavLinkProps } from './../types/components/navlink'
+
+    const props = defineProps({
+        navLinks: Array as () => INavLinkProps[],
+    });
+</script>
+
+<style scoped>
+    .navigator {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 20px;
+      background-color: #333;
+      height: 40px;
+    }
+</style>
+  
