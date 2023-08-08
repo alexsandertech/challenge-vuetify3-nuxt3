e28313e9876a1e35de9225afcc27dfad0e992420
Alexsander Barbosa <alexsander.barbosa@hotmail.com>
2023-08-08 19:01:27 -0300

    feat: layout with menu

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="default">
+        <NuxtPage />
+        Page [Home] 
+    </div>
+</template>
+
+<style scoped>
+    .default {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        align-self: center;
+        align-content: center;
+        padding: 10px;
+    }
+</style>
