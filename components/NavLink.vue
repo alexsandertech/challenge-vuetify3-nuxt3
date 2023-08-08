dc19f253b282476e52966e50ce97fa92784af925
Alexsander Barbosa <alexsander.barbosa@hotmail.com>
2023-08-08 19:01:27 -0300

    feat: layout with menu

---
+++ components/NavLink.vue
@@ -0,0 +1,37 @@
+<template>
+    <NuxtLink :to="to" class="n-link-base nav-item" :class="selectedClass">
+      <slot></slot>
+    </NuxtLink>
+</template>
+  
+<script setup lang="ts">
+    import { useRouter } from 'vue-router';
+    import { computed } from 'vue';
+  
+    const props = defineProps({
+      to: String,
+    });
+  
+    const router = useRouter();
+  
+    const selectedClass = computed(() => {
+      return router.currentRoute.value.path === props.to ? 'nav-item-selected' : '';
+    });
+</script>
+  
+<style scoped>
+    .nav-item {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: white;
+        text-decoration: none;
+        height: 100%;
+        padding: 0px 8px 0px 8px;
+    }
+  .nav-item-selected {
+    background-color: #04AA6D;
+    color: black;
+  }
+</style>
+  
---
+++ layouts/default.vue
@@ -0,0 +1,51 @@
+<template>
+    <Navigator :navLinks="navLinks" />
+    <NuxtLoadingIndicator />
+    <v-container class="bg-surface-variant default">
+        <ResetCss>
+            <slot></slot>
+        </ResetCss>
+    </v-container>
+    <footer>
+      <div class="text-center p-4 op-50">
+        Current route: <code>{{ router.currentRoute.value.path }}</code>
+      </div>
+      <button @click="setPageLayout('default')">
+        layouts/default.vue
+      </button>
+      <!-- <button @click="setPageLayout('')">
+        Remove layout
+      </button> -->
+    </footer>
+</template>
+
+<script setup lang="ts">
+    import Navigator from './../components/Navigator.vue';
+    import { useRouter } from 'vue-router';
+
+    const router = useRouter();
+
+    const props = defineProps({
+        navLinks: Array as () => INavLinkProps[],
+    });
+</script>
+
+<style scoped>
+  .default {
+    display: flex;
+    height: calc(100vh - 40px);
+    width: 100%;
+    border: 1px solid #cfcfcf;
+    padding: 1rem;
+    position: relative;
+    background-color: #fff !important;
+  }
+  .default::before {
+    content: 'layouts|default.vue';
+    position: absolute;
+    top: 2px;
+    left: 5px;
+    color: #ddd;
+    font-family: monospace;
+  }
+</style>
