e2272f16da9cff1ab5e029c1bbb4c545b0175cf7
Alexsander Barbosa <alexsander.barbosa@hotmail.com>
2023-08-07 22:41:29 -0300

    Feature/initial setup (#1)
    * feat: initial setup nuxt
    
    * chore: vuetify
    
    * chore: @mdi/font vite-plugin-vuetify
    
    * feat: component example

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="display: flex; width: 100vw; height: 100vh; justify-content: center; align-items: center;">
+    <v-card
+      class="mx-auto"
+      width="400"
+      prepend-icon="mdi-home"
+    >
+      <template v-slot:title>
+        Test component
+      </template>
+
+      <v-card-text>
+        This is content
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
